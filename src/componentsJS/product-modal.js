462d234d71d362d1a8972df94c7caf8d4db731d6
Svetlana <ternovskaya.svetlana89@gmail.com>
2020-12-12 12:02:23 +0200

    after merging

--- src/componentsJS/product-modal.js
+++ src/componentsJS/product-modal.js
@@ -1,17 +1,16 @@
 import ApiService from './apiService';
 const apiService = new ApiService();
 import productModalCard from '../partials/product-modal.handlebars';
+import refs from '../js/refs';
 
-const refs = {
-  productContainer: document.querySelector('.product-container'),
-}
+console.log(refs.productContainer);
 
-apiService.fetchProductData().then(data => {
-	const products = data.work;
-	const product = products.find(product => product._id === "5fd27c020031930017e917a0");
-	const markup = productModalCard(product);
-	// console.log(markup);
-	console.log(refs.productContainer);
-  refs.productContainer.insertAdjacentHTML('beforeend', markup);
-})
+// apiService.fetchProductData().then(data => {
+// 	const products = data.work;
+// 	const product = products.find(product => product._id === "5fd27c020031930017e917a0");
+// 	const markup = productModalCard(product);
+// 	// console.log(markup);
+// 	console.log(refs.productContainer);
+//   refs.productContainer.insertAdjacentHTML('beforeend', markup);
+// })
 
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,7 @@
 import refs from './renderComponentsHTML'
 
 refs.MyCallSavaBtn = document.querySelector('.my-calls-btn-org')
+refs.productContainer = document.querySelector('.js-product')
 
 
 export default refs;
