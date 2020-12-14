0e07968484292c23155d8494bb30c310b24c4afc
MykhailoOtmakhov <65648953+MykhailoOtmakhov@users.noreply.github.com>
2020-12-14 22:04:01 +0200

    муки с запросом

--- src/js/authModal.js
+++ src/js/authModal.js
@@ -26,40 +26,37 @@ email.addEventListener("input", function (event) {
 
 // const BASE_URL = 'https://callboard-backend.herokuapp.com';
 
-// const registerUser = userData => {
-//     const { email, password } = userData;
-//     return axios.post(`${BASE_URL}/auth/register`, {email, password});
-// }
-// const postToAdd = {
-//   email: document.querySelector('#mail'),
-//   password: document.querySelector('#password')
-// };
+const registerUser = userData => {
+    const { email, password } = userData;
+    return axios.post(`${BASE_URL}/auth/login`, {email, password});
+}
+// const logInButton = document.querySelector('.log-in')
+const registerFormRef = document.querySelector('.auth-modal-form');
+//const emailRef = document.querySelector('.email');
+//const passwordRef = document.querySelector('.password');
+const handleRegisterSubmit = event => {
+    event.preventDefault();
+    const { target: form } = event;
+    // const { emailRef, passwordRef } = event;
+    // const { currentTarget: form } = event.target;
+    // const { registerFormRef } = event;
+    const formData = new FormData(form);
+    // const formData = new FormData(emailRef, passwordRef);
+    const body = {}
 
+    formData.forEach((value, key)=> {
+        body[key] = value;
+    })
 
+    // )
+    // registerUser(event)
+    registerUser(body)
+        .then(({data}) => console.log(data))
+        // .then(({emailRef, passwordRef}) => console.log(emailRef, passwordRef))
+}
 
-// const registerFormRef = document.querySelector('.auth-modal-form');
-// // console.log(registerFormRef)
-// const handleRegisterSubmit = event => {
-//     event.preventDefault();
-
-//     const { currentTarget: form } = event;
-//     const formData = new FormData(form);
-//     const body = {}
-
-
-//     formData.forEach((value, key)=> {
-//         body[key] = value;
-//     })
-//     console.log(body)
-//     // console.log(formData);
-//     // formData.forEach((value, key) => {
-//     //     console.log(key.value);
-//     // })    
-//     registerUser(body)
-//         .then(result => console.log(result))
-// }
-
-// registerFormRef.addEventListener('submit', handleRegisterSubmit)
+// logInButton.addEventListener('click', handleRegisterSubmit)
+registerFormRef.addEventListener('submit', handleRegisterSubmit)
 
 
 
