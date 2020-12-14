40e942066c8cd0024f59b2fa3884d24335c2d306
Roman Dobrodii <romariosi007@gmail.com>
2020-12-15 00:55:05 +0200

    Валидация ломает верстку

--- src/componentsJS/addModal.js
+++ src/componentsJS/addModal.js
@@ -21,6 +21,12 @@ function onOpenAddModal() {
 function onCloseAddModal() {
   window.removeEventListener('keydown', onEscKeyPress);
   document.body.classList.remove('show-modal');
+  let formReq = refs.form.querySelectorAll('._req');
+  for (let i = 0; i < formReq.length; i++) {
+    const element = formReq[i];
+    element.classList.remove('visible');
+  }
+  // form.reset();
 }
 
 function onBackdropClick(event) {
@@ -113,3 +119,126 @@ fetch('https://callboard-backend.herokuapp.com/call/categories')
     refs.selectorCategory.insertAdjacentHTML('beforeend', markup);
   })
   .catch(error => console.log('error', error));
+
+//===========================================
+// const formdata = new FormData();
+const form = refs.form.addEventListener('submit', formSend);
+
+async function formSend(e) {
+  e.preventDefault();
+
+  let error = addFormValidate(form);
+
+  // const accessToken = sessionStorage.getItem('token');
+
+  if (error === 0) {
+    //   var myHeaders = new Headers();
+    //   myHeaders.append('Authorization', `Bearer ${accessToken}`);
+    //   let formReq = refs.form.querySelectorAll('._req');
+    //   for (let i = 0; i < formReq.length; i++) {
+    //     const element = formReq[i];
+    //     if (element.classList.contains('_name')) {
+    //       formdata.append('title', `${element.value}`);
+    //     } else if (element.classList.contains('_description')) {
+    //       formdata.append('description', `${element.value}`);
+    //     } else if (element.classList.contains('_phone')) {
+    //       formdata.append('phone', `${element.value}`);
+    //     } else if (element.classList.contains('_category')) {
+    //       let value = element.options[element.selectedIndex].value;
+    //       formdata.append('category', `${value}`);
+    //     } else if (element.classList.contains('_price')) {
+    //       formdata.append('price', `${element.value}`);
+    //     }
+    //   }
+    //   var requestOptions = {
+    //     method: 'POST',
+    //     headers: myHeaders,
+    //     body: formdata,
+    //     redirect: 'follow',
+    //   };
+    //   fetch('https://callboard-backend.herokuapp.com/call/', requestOptions)
+    //     .then(response => response.json())
+    //     .then(result => {
+    //       console.log(result);
+    //       onCloseAddModal();
+    //     })
+    //     .catch(error => console.log('error', error));
+  } else {
+    alert('Заповніть будь-ласка всі поля');
+  }
+}
+
+// addImage.addEventListener('change', () => {
+//   uploadFile(addImage.files[0]);
+//   formdata.append('file', addImage.files[0]);
+// });
+
+// imageList.addEventListener('click', e => {
+//   if (e.target.tagName === 'IMG') {
+//     const remove = confirm('Ви впевнені що хочете видалити фото зі списку?');
+//     if (remove) {
+//       e.target.parentNode.remove();
+//     }
+//   }
+// });
+
+function addFormValidate(form) {
+  let error = 0;
+  let formReq = refs.form.querySelectorAll('._req');
+
+  const price = document.querySelector('._price');
+  const errorPriceBlock = document.querySelector('.error-price');
+
+  errorPriceBlock.classList.remove('visible');
+
+  for (let i = 0; i < formReq.length; i++) {
+    const element = formReq[i];
+    element.classList.remove('visible');
+
+    if (element.classList.contains('_name')) {
+      if (element.value.length <= 3 || element.value.length >= 15) {
+        element.classList.add('visible');
+        error += 1;
+      }
+    } else if (element.classList.contains('_description')) {
+      if (element.value.length <= 5) {
+        element.classList.add('visible');
+        error += 1;
+      }
+    } else if (element.classList.contains('_phone')) {
+      if (phoneValidate(element)) {
+        element.classList.add('visible');
+        error += 1;
+      }
+    } else if (element.classList.contains('_category')) {
+      let value = element.options[element.selectedIndex].value;
+      if ((value === 'work' || value === 'free') && price.value !== '0') {
+        errorPriceBlock.classList.add('visible');
+      }
+
+      if (element.value === 'category') {
+        element.classList.add('visible');
+        error += 1;
+      }
+    } else if (element.classList.contains('_price')) {
+      if (
+        (!Number(element.value) && Number(element.value) !== 0) ||
+        element.value === ''
+      ) {
+        element.classList.add('visible');
+        error += 1;
+      }
+    } else if (
+      refs.imgLoaderArea.children.length < 1 ||
+      refs.imgLoaderArea.children.length > 5
+    ) {
+      element.classList.add('visible');
+      error += 1;
+    }
+  }
+  return error;
+}
+
+function phoneValidate(element) {
+  return !/^\+380\d{3}\d{2}\d{2}\d{2}$/.test(element.value);
+}
