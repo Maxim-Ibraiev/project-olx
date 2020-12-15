5a35b2626fe4f503dca4cfee38169dd0add4b644
Roman Dobrodii <romariosi007@gmail.com>
2020-12-16 00:10:52 +0200

    Добавляет валидацию модалки добавить объявление.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import { createPopper } from '@popperjs/core';
-import bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.scss';  
+import { createPopper } from '@popperjs/core';
+import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss';
 import './js/prodCard';
 import './js/renderComponentsHTML';
-import fetchCategories from './js/fetchCategories'
+import fetchCategories from './js/fetchCategories';
 import fullscreen from './images/fullscreen.png';
 import heart from './images/heart.png';
 import heartfill from './images/heartfill.png';
 // import './js/cardAddToFavorites'
-import './componentsJS/addModal';
+// import './componentsJS/addModal';
+import './componentsJS/add-modal-logic/add-modal-open';
+import './componentsJS/add-modal-logic/add-modal-close';
+import './componentsJS/add-modal-logic/add-modal-upload-file';
+import './componentsJS/add-modal-logic/add-modal-send';
+import './componentsJS/add-modal-logic/add-modal-validation';
 import './componentsJS/myCalls';
 // import './componentsJS/prodCard';
 
-
 // import './js/exampleBackend'
 
-import './js/menu'
+import './js/menu';
