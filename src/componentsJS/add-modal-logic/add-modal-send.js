2399e8f4bdc3776eb9fa9a158c213aca2736ce32
Roman Dobrodii <romariosi007@gmail.com>
2020-12-19 22:32:32 +0200

    чистит мусор

--- src/componentsJS/add-modal-logic/add-modal-send.js
+++ src/componentsJS/add-modal-logic/add-modal-send.js
@@ -54,27 +54,7 @@ imageList.addEventListener('click', e => {
     }
   }
 });
-//=========================================================================
-// import uploadFile from './add-modal-upload-file';
-// import refs from './refs';
-// import formValidate from './add-modal-validation';
-// import { handleCloseModal } from './add-modal-close';
-
-// const { form, addImage, imageList } = refs;
-// const formdata = new FormData();
-
-// form.addEventListener('submit', formSend);
-
-// async function formSend(e) {
-//   e.preventDefault();
-
-//   let error = formValidate(form);
-//   const accessToken = sessionStorage.getItem('token');
-
-//   if (error === 0) {
-//     var myHeaders = new Headers();
-//     myHeaders.append('Authorization', `Bearer ${accessToken}`);
-
+//===================================================================
 //     let formReq = form.querySelectorAll('._req');
 //     for (let i = 0; i < formReq.length; i++) {
 //       const element = formReq[i];
@@ -92,36 +72,3 @@ imageList.addEventListener('click', e => {
 //         formdata.append('price', `${element.value}`);
 //       }
 //     }
-
-//     var requestOptions = {
-//       method: 'POST',
-//       headers: myHeaders,
-//       body: formdata,
-//       redirect: 'follow',
-//     };
-
-//     fetch('https://callboard-backend.herokuapp.com/call/', requestOptions)
-//       .then(response => response.json())
-//       .then(result => {
-//         console.log(result);
-//         handleCloseModal();
-//       })
-//       .catch(error => console.log('error', error));
-//   } else {
-//     alert('Заповніть будь-ласка всі поля!');
-//   }
-// }
-
-// addImage.addEventListener('change', () => {
-//   uploadFile(addImage.files[0]);
-//   formdata.append('file', addImage.files[0]);
-// });
-
-// imageList.addEventListener('click', e => {
-//   if (e.target.tagName === 'IMG') {
-//     const remove = confirm('Ви впевнені що бажаєте видалити фото зі списку?');
-//     if (remove) {
-//       e.target.parentNode.remove();
-//     }
-//   }
-// });
