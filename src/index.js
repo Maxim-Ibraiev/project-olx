a46ff74f0414a1fdcc106949858799daf0d2bf27
Alexey Golodenko <alexey.golodenko@gmail.com>
2020-12-21 09:27:27 +0200

    фикс

--- src/index.js
+++ src/index.js
@@ -26,4 +26,4 @@ import './js/menu';
 // import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
 //import './js/cardAddToCategory'
 import './js/main';
-// import './js/categories';
+//import './js/categories';
